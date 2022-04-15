b0e9dd59d6e5f2df2b8cfdbb5ffbf752f490dbf6
limsangjae <dlatkdwoa@gmail.com>
2022-04-15 16:22:37 +0900

    시큐리티 세팅

--- src/main/java/com/LSJ/Blog/config/SecurityConfig.java
+++ src/main/java/com/LSJ/Blog/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.LSJ.Blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -18,10 +22,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .mvcMatchers("members/join").anonymous()
                 )
                 .formLogin()
-                    .loginPage("members/login")
-                    .loginProcessingUrl("members/doLogin")
+                    .loginPage("/members/login")
+                    .loginProcessingUrl("/members/doLogin")
                     .usernameParameter("loginId")
                     .passwordParameter("longinPw")
                     .defaultSuccessUrl("/")
+            .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                    .logoutUrl("/")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+                    .clearAuthentication(true)
+            .and()
+                .sessionManagement()
+                    .invalidSessionUrl("/")
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
+                        .expiredUrl("/");
+
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
